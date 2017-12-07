20594738760a49553efd09efdc938f85d8e0d690
Kerin Khan <kerink585@gmail.com>
2017-12-06 18:19:21 -0800

    fixed: bar color, bar limit off screen

--- keepBallUp.js
+++ keepBallUp.js
@@ -1,23 +1,54 @@
 var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
+
 var bar;
 var ball;
 var score=0;
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-c.font = "30px Arial";
-c.fillStyle="red";
-c.fillText("Keep the Ball Up",innerWidth/4,40);
+var c = canvas.getContext('2d');
+var ctx= canvas.getContext('2d');
+ctx.font = "30px Arial";
+ctx.fillStyle="red";
+ctx.fillText("Keep the Ball Up",innerWidth/4,40);
 var colors = ['#2C3E50','#FC4349','#D7DADB','#6DBCDB','#FFFFFF'];
+
 window.addEventListener('keydown',function(event){
 	canvas.key= event.keyCode;
 })
-window.addEventListener('keyup',function(){
+window.addEventListener('keyup',function(event){
 	canvas.key=false;
 })
 
-function Circle(x,y,radius,dx,dy){
+
+function Rectangle(x,y,color,width,height){
+	this.x=x;
+	this.y=y;
+	this.color=color;
+	this.width=width;
+	this.height=height;
+
+	this.drawRectangle=function(){
+		this.fillStyle=this.color;
+		c.fillRect(this.x,this.y,this.width,this.height);
+	}
+	this.updateRectangle = function(){
+		if(this.x>innerWidth-100 || this.x<0){
+			this.x=-this.x;
+			
+		}
+		if(canvas.key&&canvas.key==37){
+			this.x-=15;
+		}
+		if(canvas.key&&canvas.key==39){
+			this.x+=15;
+		}
+		this.drawRectangle();
+	}
+
+}
+
+function Circle(x,y,radius,dx,dy,isCircle){
 	this.x=x;
 	this.y=y;
 	this.radius=radius;
@@ -26,23 +57,16 @@ function Circle(x,y,radius,dx,dy){
 	this.color=colors[Math.floor(Math.random()*colors.length)];
 
 	this.drawCircle=function(){
-		c.beginPath();
-		c.arc(this.x,this.y,this.radius,0,2*Math.PI);
-		c.fillStyle = this.color;
-		c.fill();
-	}
-	this.updateCircle=function(){
-		if((this.x+this.radius)>innerWidth || (this.x-this.radius)<0){
-			this.dx=-this.dx;
-		}
-		else if(this.y+this.radius>innerHeight || this.y-this.radius<0){
-			this.dy=-this.dy;
+		if(isCircle){
+			c.beginPath();
+			c.arc(this.x,this.y,this.radius,0,2*Math.PI);
+			c.fillStyle = this.color;
+			c.fill();
 
 		}
-		this.drawCircle();
-		this.x+=this.dx;
-		this.y+=this.dy;
+
 	}
+
 	this.collision = function(){
 		var barline = bar.y-this.height;
 
@@ -60,48 +84,45 @@ function Circle(x,y,radius,dx,dy){
 			}
 
 	}
+		this.updateCircle=function(){
+		if((this.x+this.radius)>innerWidth || (this.x-this.radius)<0){
+			this.dx=-this.dx;
+		}
+		else if(this.y+this.radius>innerHeight || this.y-this.radius<0){
+			this.dy=-this.dy;
+
+		}
+		//this.collision();
+		this.drawCircle();
+		this.x+=this.dx;
+		this.y+=this.dy;
+	}
 
 
 }
 
 
-function Rectangle(x,y,color,width,height){
-	this.x=x;
-	this.y=y;
-	this.color=color;
-	this.width=width;
-	this.height=height;
 
-	this.drawRectangle=function(){
-		this.fillStyle=this.color;
-		c.fillRect(this.x,this.y,this.width,this.height);
-	}
-	this.updateRectangle = function(){
-		if(canvas.key&&canvas.key==37){
-			this.x-=10;
-		}
-		if(canvas.key&&canvas.key==39){
-			this.x+=10;
-		}
-		this.drawRectangle();
-	}
-}
 
 var ballx= Math.random()*innerWidth;
 var bally= Math.random()*innerHeight;
-bar = new Rectangle(innerWidth/4,innerHeight-30,'red',150,10);
-ball = new Circle(ballx,bally,30,4,4);
+bar = new Rectangle(innerWidth/4,innerHeight-30,'blue',150,20);
+ball = new Circle(ballx,bally,30,4,4,true);
 
 function animate(){
 	/*this function repeatedly calls animate function*/
 	console.log('score',score);
+	ctx.font="30px Arial";
+    ctx.fillStyle="#FC4349"
+    ctx.fillText(score,10,50);
 	
 	requestAnimationFrame(animate);
 	c.clearRect(0,0,innerWidth,innerHeight);
 	ball.collision();
+	bar.updateRectangle();
 	ball.updateCircle();
 
-	bar.updateRectangle();
+	
 	
 
 
