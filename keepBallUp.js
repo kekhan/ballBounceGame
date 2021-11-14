7adf5e4a18cdfe329a6bedbf93a568b5d9ffc2fc
Kerin Khan <kerink585@gmail.com>
2021-11-13 21:46:22 -0800

    Update keepBallUp.js

--- keepBallUp.js
+++ keepBallUp.js
@@ -14,8 +14,8 @@ fetch(host + "/highscore").then(resp => resp.json()).then(obj => {
 	console.log(obj)
 })
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+canvas.height = 500px;
+canvas.width = 600px;
 var c = canvas.getContext('2d');
 var ctx= canvas.getContext('2d');
 ctx.font = "30px Arial";
